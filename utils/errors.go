91144e9aee46020232035c3556b9ac970d07958d
SuInk <xilei08004@qq.com>
2023-01-23 22:16:05 +0800

    add user-agent

--- utils/errors.go
+++ utils/errors.go
@@ -3,7 +3,7 @@ package utils
 import "errors"
 
 var (
-	ERROR_UNIFIED    = errors.New("学校统一认证服务出现错误，请稍后再试")
+	ERROR_UNIFIED    = errors.New("统一认证服务出现问题，请稍后再试")
 	ERROR_SERVER     = errors.New("服务器出了问题，请稍后再试")
 	ERROR_ID_PWD     = errors.New("账号或者密码错误,请确认能通过统一认证服务登录教务系统")
 	ERROR_CAPTCHA    = errors.New("验证码识别错误，,重试一下？")
